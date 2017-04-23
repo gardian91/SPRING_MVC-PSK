43667acb7323440e3c6cd1df3c8f2fbcb2e7a61c
Paweł Łagan <pawel.lagan@gmail.com>
2017-04-23 17:14:33 +0200

    part2

--- complete/src/main/java/com/psk/bank/configuration/Jackson.java
+++ complete/src/main/java/com/psk/bank/configuration/Jackson.java
@@ -1,13 +1,21 @@
 package com.psk.bank.configuration;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class Jackson {
+public class Jackson extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new Base64ImageHttpMessageConverter());
+    }
 
     @Bean
     public Jackson2ObjectMapperBuilder jacksonBuilder() {
@@ -15,4 +23,5 @@ public class Jackson {
         b.indentOutput(true).dateFormat(new SimpleDateFormat("yyyy-MM-dd"));
         return b;
     }
+    
 }
--- complete/src/main/java/com/psk/bank/configuration/MvcConfig.java
+++ complete/src/main/java/com/psk/bank/configuration/MvcConfig.java
@@ -3,6 +3,7 @@ package com.psk.bank.configuration;
 import java.text.SimpleDateFormat;
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.converter.HttpMessageConverter;
